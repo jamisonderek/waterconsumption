a98646acccab8460ff8aa263f5c1d895b3673645
Derek Jamison <derek@jamisoncreations.com>
2021-05-13 19:18:31 -0500

    Update the data for the simulated device every 2 seconds

--- simulateddevice.py
+++ simulateddevice.py
@@ -1,4 +1,6 @@
 from iotdevice import IotDevice
+import asyncio
+import random
 
 class SimulatedDevice(IotDevice):
     def __init__(self):
@@ -7,6 +9,9 @@ class SimulatedDevice(IotDevice):
         IotDevice.set_humidity(self, 30.0)
         IotDevice.set_moisture(self, 4.0)
         IotDevice.set_temperature(self, 71.1)
+        # spawn a task to update our data
+        __update_task = asyncio.create_task(self.update_loop())
+        # TODO: Cancel this task if our object gets deleted.
 
     def turn_valve_on(self):
         IotDevice.set_flow(self, 6.2)
@@ -15,3 +20,14 @@ class SimulatedDevice(IotDevice):
     def turn_valve_off(self):
         IotDevice.set_flow(self, 0.0)
         IotDevice.turn_valve_off(self)
+
+    def update_data(self):
+        IotDevice.set_light(self, random.random()*10.0)
+        IotDevice.set_moisture(self, random.random()*10.0)
+        IotDevice.set_humidity(self, random.random()*100.0)
+        IotDevice.set_temperature(self, 40.0+random.random()*40.0)
+
+    async def update_loop(self):
+        while True:
+            self.update_data()
+            await asyncio.sleep(2)
