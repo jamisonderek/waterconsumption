62ccb62dd391b113f7d50b0fc96d757046caf098
Derek Jamison <derek@jamisoncreations.com>
2021-05-13 19:14:19 -0500

    call get_telemetry.  format floating numerical output

--- testdevice.py
+++ testdevice.py
@@ -15,11 +15,13 @@ async def testDevice(device):
     print ("Waiting 10 seconds to allow device to initialize.")
     await asyncio.sleep(10)
 
+    print(device.get_telemetry())
+
     print ("Temperature is {0:.2f}F".format(device.temperature))
-    print ("Humidity is {0} out of 100".format(device.humidity))
-    print ("Light is {0} out of 10".format(device.light))
-    print ("Moisture is {0} out of 10".format(device.moisture))
-    print ("Flow is {0} Liters/minute".format(device.flow))
+    print ("Humidity is {0:.2f} out of 100".format(device.humidity))
+    print ("Light is {0:.2f} out of 10".format(device.light))
+    print ("Moisture is {0:.2f} out of 10".format(device.moisture))
+    print ("Flow is {0:.2f} Liters/minute".format(device.flow))
     print ("Valve is {0}".format(device.valve))
 
     print ("Turning on valve...")
@@ -40,6 +42,12 @@ async def testDevice(device):
     print ("Valve is {0}".format(device.valve))
     print ("Flow is {0} Liters/minute".format(device.flow))
 
+    print ("Temperature is {0:.2f}F".format(device.temperature))
+    print ("Humidity is {0:.2f} out of 100".format(device.humidity))
+    print ("Light is {0:.2f} out of 10".format(device.light))
+    print ("Moisture is {0:.2f} out of 10".format(device.moisture))
+    print ("Flow is {0:.2f} Liters/minute".format(device.flow))
+    print ("Valve is {0}".format(device.valve))
 
 if __name__ == "__main__":
     asyncio.run(main())
